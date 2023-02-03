ad32c522295e2fcffdf606710578dc2c4a051f42
unknown <https://github.com/ArmenManukyan12/relqone.git>
2023-02-03 11:34:36 +0400

    arm

---
+++ Armen/js/script.js
@@ -0,0 +1,27 @@
+  let boxes = document.querySelectorAll(".boxes");
+  let box = document.querySelectorAll("#container");
+
+  function slot() {
+    const a = [7 ,7 ,7] ;
+
+
+    const newDiv = document.createElement("div");
+    const button = document.createElement("button");
+    button.textContent = "PLAY";
+    newDiv.textContent = `${a} `;
+
+    button.addEventListener("click", () => {
+      newDiv.textContent = `${Math.floor(Math.random() * 10)} 
+                            ${Math.floor(Math.random() * 10)} 
+                            ${Math.floor(Math.random() * 10)}
+                            `;
+    });
+
+    return [newDiv, button];
+  }
+
+  for (let i = 0; i < boxes.length; i++) {
+    const [div, button] = slot();
+    boxes[i].append(div, button);
+
+  }
